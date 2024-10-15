512a10713b588874e090263e0c563e8516d233f2
枫 <fx20031215@163.com>
2024-10-15 15:26:15 +0800

    feat: 初始化枫券

---
+++ distribution/src/main/java/com/fengxin/DistributionApplication.java
@@ -0,0 +1,17 @@
+package com.fengxin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author FENGXIN
+ * @date 2024/10/15
+ * @project Default (Template) Project
+ * @description 分发服务启动类
+ **/
+@SpringBootApplication
+public class DistributionApplication {
+    public static void main (String[] args) {
+        SpringApplication.run(DistributionApplication.class, args);
+    }
+}
---
+++ settlement/src/main/java/com/fengxin/SettlementApplication.java
@@ -0,0 +1,17 @@
+package com.fengxin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author FENGXIN
+ * @date 2024/10/15
+ * @project Default (Template) Project
+ * @description
+ **/
+@SpringBootApplication
+public class SettlementApplication {
+    public static void main (String[] args) {
+        SpringApplication.run(SettlementApplication.class, args);
+    }
+}
