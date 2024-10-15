e393a14d0bcbc23e09bbbad83fe7ab34f6d1ac86
枫 <fx20031215@163.com>
2024-10-15 15:37:09 +0800

    feat: 初始化商家后台管理系统和图标

---
+++ merchant-admin/src/main/java/com/fengxin/MerchantAdminApplication.java
@@ -0,0 +1,13 @@
+package com.fengxin;
+
+/**
+ * @author FENGXIN
+ * @date 2024/10/15
+ * @project Default (Template) Project
+ * @description
+ **/
+public class MerchantAdminApplication {
+    public static void main (String[] args) {
+        System.out.println ("Hello, World!");
+    }
+}
