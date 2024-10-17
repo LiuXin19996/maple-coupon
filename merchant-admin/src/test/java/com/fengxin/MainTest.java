f941d0c40a07dd8fb6b2c684401c4455ee6b898e
枫 <fx20031215@163.com>
2024-10-17 23:52:55 +0800

    feat: 添加自定义分片算法分库分表配置
    解决数据不均匀

---
+++ merchant-admin/src/test/java/com/fengxin/MainTest.java
@@ -0,0 +1,18 @@
+package com.fengxin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+/**
+ * @author FENGXIN
+ * @date 2024/10/17
+ * @project feng-coupon
+ * @description
+ **/
+@SpringBootTest
+public class MainTest {
+    @Test
+    void contextLoads() {
+        System.out.println ("...");
+    }
+}
