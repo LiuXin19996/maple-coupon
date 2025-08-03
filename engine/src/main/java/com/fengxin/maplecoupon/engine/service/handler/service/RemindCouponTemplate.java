5aa141b55cb5de173c22c12d9ffae48933e098c8
maple <fxin991@gmail.com>
2025-08-04 00:32:31 +0800

    1

--- engine/src/main/java/com/fengxin/maplecoupon/engine/service/handler/service/RemindCouponTemplate.java
+++
@@ -1,8 +0,0 @@
-package com.fengxin.maplecoupon.engine.service.handler.service;
-
-import com.fengxin.maplecoupon.engine.service.handler.dto.CouponTemplateRemindDTO;
-
-public interface RemindCouponTemplate {
-    
-    void remind (CouponTemplateRemindDTO couponTemplateRemindDTO);
-}
